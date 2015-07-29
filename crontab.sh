2f5e6107001a1d4fb97dc701b5955d4fe374950c
undefined <undefined@xpure.com>
2015-07-29 05:44:54 +0000

    f

--- crontab.sh
+++ crontab.sh
@@ -1,7 +1,7 @@
 PREFIX=$(cd "$(dirname "$0")"; pwd)
 cd $PREFIX
 source ~/.bashrc
-
+date >> /var/log/sitemap/sitemap.log
 python $PREFIX/sitemap/sitemap.py >> /var/log/sitemap/sitemap.log 2>&1
 crontab -l > $PREFIX/crontab
 
